a0adc15f6805f2ed2dce0d2f1e9593ea9711bda5
Luksys5 <luksys5@yahoo.com>
2014-02-03 14:04:40 +0200

    Update Naujas.java

--- Naujas.java
+++ Naujas.java
@@ -1,3 +1,26 @@
-String gitUser = "newGitUSER"
-gitUser.split("Git");
-System.out.println(gitUser);
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Delimiter {
+    static String sakau = null;
+    static String[] newput = new String[2];
+    public static void main(String[] args) throws   IOException{
+            Scanner sc = new Scanner(new FileReader("labas.txt"));
+            sc.useDelimiter("kuo");
+
+                System.out.println(sc.next());
+                sakau = sc.next();
+                
+                newput = sakau.split("\\s+\\d+");
+
+                System.out.println(newput[1]);
+          
+    }
+    
+}
