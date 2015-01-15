7d3c6c6d23194f592d9b61a35d4a218cedb91ae9
Lukas <lt.tutkus7@gmail.com>
2015-01-15 16:35:34 +0200

    Midified...

--- cols.py
+++ cols.py
@@ -1,53 +1,88 @@
 #Nurodo pdb strukturos alfa beta strukturas pakeiciant spalva 
 #Skirtingi failai perduodami ciklui
 #vieni trumpi tik vieno baltymo, kiti keliu baltymu
-def spalv(dssp,notdssp,output,count):
+def spalv(dssp,notdssp,output,count,outkons):
 	lines = dssp.readlines()
-	oneline =''
-	mt = 0
-	alfa = []
-	beta = []
+	betastr = ''
+	alfastr = ''
+	pavadinimas=''
+	alen = 0
+	blen = 0
 	for y in lines:
                	y = y.rstrip('\n')
                	read = notdssp.readline()
                	if('>' in read):
+			if alfastr != 0 and betastr != 0:
+				betastr += ' '+str(blen)
+				alfastr += ' '+str(alen)
+			blen = 0
+			alen = 0
+			pavadinimas += read[1:]
+			betastr += bcolors.RESET+'\n'+bcolors.YELLOW+'beta'
+			outkons.write('\n'+read[1:])
+			alfastr += bcolors.RESET+'\n'+bcolors.GREEN+'alfa'
 			length = 0
-			mt = 0
-			if beta != []:
-				output.write(x for x in beta )
-				output.write('\n')
-				return
 			output.write('\n')
-                       	output.write(bcolors.RESET+read)
-			alfa = []
-			beta = []
-               	else:
-			output.write('\n')
+                       	output.write(bcolors.RESET+read[1:].rstrip('\n'))
+               		beta = 0
+			alfa = 0
+		else:
 			for i in range(0,len(y)):
 				output.write(bcolors.BLUE)
-				output.write(str(i+length)+" "*len(str(i)))
+				output.write(str(i+1+length)+" "*len(str(i)))
 				output.write(bcolors.RESET)
 			read = read.rstrip('\n')
-			output.write('\n')
-        		for i in range(0,len(y)):
+        		output.write('\n')
+			for i in range(0,len(y)):
                 	        if( y[i] == '-'):
                 			output.write(bcolors.RESET)
-					if len(beta) % 2 == 1:
-					#	beta[len(beta)-1] = str(beta[len(beta)-1]) + str(mt-1)
-						beta.append(str(mt-1)+" ")
+					if( y[i-1] == 'E'):
+						betastr += ('-'+str(length+i))
+						outkons.write(('-'+str(length+i)))
+					if( y[i-1] == 'H' and i >0):
+                                                outkons.write('-'+str(length+i)+' ')
+						alfastr += ('-'+str(int(length+i))) 
+					beta = 0
+					alfa = 0
                 		elif( y[i]=='E'):
-					if len(beta) % 2 == 0:
-						beta.append(str(mt)+"-")
+					if beta == 0:
+						if alfa == 1:
+							outkons.write('-'+str(length+i)+' ')
+                                                	alfastr += ('-'+str(int(length+i)))
+						blen += 1
+						beta = 1
+						betastr += ' ' + str(length+i+1)
+						outkons.write(' '+str(length+i+1))
                 	        	output.write(bcolors.YELLOW)
+					alfa = 0
                 	       	else:
+					beta = 0
                 	        	output.write(bcolors.GREEN)
-				#	if len(beta) % 2 == 1:
-                                 #               beta[len(beta)-1] = str(beta[len(beta)-1]) + str(mt)
-				mt +=1 
-				output.write(read[i]+" "*(len(str(i+length))+(len(str(i))-len(read[i]))))
+					if alfa == 0:
+						if beta == 1:
+                                                        outkons.write('-'+str(length+i)+' ')
+                                                        betastr += ('-'+str(int(length+i)))
+						alen += 1
+						alfa = 1
+						alfastr += ' ' + str(length+i+1)
+				output.write(read[i]+" "*(len(str(i+length))+(len(str(i+1))-len(read[i]))))
 			length += len(y)
-		oneline = ''
+			#utput.write('\nbeta '+bcolors.RESET+betastr)
               	output.write('\n')
+	#output.write(betastr)
+	#output.write(alfastr)
+	alfal = alfastr.split('\n')
+	betal = betastr.split('\n')
+	alfal = alfal[1:]
+	betal = betal[1:]
+	pavlist = pavadinimas.split('\n')
+	for i in range(0, len(betal)):
+		try:
+			output.write(pavlist[i]+'\n')
+			output.write(betal[i]+'\n')
+			output.write(alfal[i]+'\n')
+		except IndexError:
+			catch = 1
 	return
 class bcolors:
 	FAIL = '\033[91m'
@@ -56,54 +91,16 @@ class bcolors:
     	GREEN = '\033[92m'
     	YELLOW = '\033[93m'
     	RESET = '\033[0m'
-	#YELLOW='\e[0;33m'
-	#BOLD='\e[4;36m'
-	#GREEN='\e[0;32m'
-	#RESET='\e[0m'
 fdssp = open('../atst/atst.dssp','r')
 fdnot = open('../atst/atst.notdssp','r')
 atstout = open('atst.colored','w')
+atstkons = open('atst.kons','w')
 atstout.write(bcolors.GREEN + ' Alfa helix H '+ bcolors.YELLOW + ' Beta strand E '+ bcolors.RESET+'\n')
-spalv(fdssp,fdnot,atstout,0)
+spalv(fdssp,fdnot,atstout,0,atstkons)
 for x in range(1,16):
 	f1 = open('../dssp/'+str(x),'r')
 	f2 = open('../notdssp/'+str(x),'r')
 	outdssp = open(str(x)+'.colored','w')
+	outkons = open(str(x)+'.kons','w')
 	outdssp.write(bcolors.GREEN + ' Alfa helix '+ bcolors.YELLOW + ' Beta strand '+ bcolors.RESET+'\n')
-	spalv(f1,f2,outdssp,x)
-	"""
-	for y in shortdssp:
-		y = y.rstrip('\n')
-		read = fdnot.readline()
-		if('>' in read):
-			outdssp.write(bcolors.RESET+read)
-		else:
-			read = read.rstrip('\n')
-			print 'yikas '+y+' length '+str(len(y))+'\nreads '+read+' length '+str(len(read))
-			for i in range(0,len(y)):
-				if( y[i] == '-'):
-					outdssp.write(bcolors.RESET)
-				elif( y[i]=='E'):
-					outdssp.write(bcolors.YELLOW)
-				else:
-					outdssp.write(bcolors.GREEN)
-				outdssp.write(read[i])
-			outdssp.write('\n')
-	for y in longdssp:
-	        y = y.rstrip('\n')
-		read = f2.readline()
-	        if('>' in y):
-			outdssp.write('\n'+ bcolors.RESET + read)
-	        else:   
-			read = read.rstrip('\n')
-	                for i in range(0,len(y)):
-	                        if( y[i] == '-'):
-	                                outdssp.write(bcolors.RESET)
-	                        elif( y[i]=='E'):
-	                                outdssp.write(bcolors.YELLOW)
-	                        else:
-	                                outdssp.write(bcolors.GREEN)
-				outdssp.write(read[i]) 
-			outdssp.write('\n')
-	outdssp.close()
-	"""
+	spalv(f1,f2,outdssp,x,outkons)
