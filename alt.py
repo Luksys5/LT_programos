8e090a82ed079ac80984a0782359567f2b613714
Luksys5 <luksys5@yahoo.com>
2015-01-23 21:41:40 +0200

    dawkd

--- alt.py
+++ alt.py
@@ -1,7 +1,7 @@
 import socket
 s = socket.socket()
-ip = socket.gethostbyname("httpbin.org")
-request = "GET /status/418 HTTP/1.1\r\nHost: httpbin.org\r\nConnection: close\r\n\r\n"
+ip = socket.gethostbyname("www8.org")
+request = "HEAD / HTTP/1.1\r\nConnection: close\r\n\r\n"
 s.connect((ip, 80))
 s.send(request)
 result = s.recv(1000)
